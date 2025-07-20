074e2f8a140d0fa4795db9df85cd98909e4de329
unknown <kcb.silvaa@outlook.com>
2025-07-19 23:07:59 -0300

    Next

--- next.config.ts
+++ next.config.ts
@@ -31,6 +31,23 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization',
+          },
+        ],
+      },
+      {
         source: '/_next/static/media/(.*)',
         headers: [
           {
